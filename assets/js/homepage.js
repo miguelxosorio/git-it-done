4d0cab10d4831b00bacbffca587272c6dda76946
vayne <miguel.osorio19@yahoo.com>
2021-12-07 17:58:01 -0600

    updated getUserRepos function, added parameter

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,19 @@
-var getUserRepos = function() {
-    console.log("function was called");
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiURL = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
     fetch("https://api.github.com/users/octocat/repos");
-  };
+
+    var response = fetch("https://api.github.com/users/octocat/repos");
+    console.log(response);
+
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+    
+};
   
   getUserRepos();

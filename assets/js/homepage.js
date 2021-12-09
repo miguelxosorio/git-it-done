9238048db195be03918cdb2b741b816d606d2d3d
vayne <miguel.osorio19@yahoo.com>
2021-12-08 22:09:00 -0600

    feature/user-repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,19 +1,111 @@
+// variables to store reference to the <form> element
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+// variables to reference the DOM elements in index.html
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiURL = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch("https://api.github.com/users/octocat/repos");
+    // fetch("https://api.github.com/users/octocat/repos");
 
-    var response = fetch("https://api.github.com/users/octocat/repos");
-    console.log(response);
+    // var response = fetch("https://api.github.com/users/octocat/repos");
 
-    fetch(apiURL).then(function(response) {
-        response.json().then(function(data) {
+    fetch(apiURL)
+    .then(function(response) {
+        // request was successful
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data) {
             console.log(data);
-        });
+            displayRepos(data, user);
+            });
+        } else {
+          alert("Error: GitHub User Not Found"); // HTTP request will still be made but instead of an attempt to display the repository data, an alert will appear
+          // can also be written as alert('Error: ' + response.statusText);
+        }
+    })
+    .catch(function(error) { // api's way of handling network errors - When we use fetch() to create a request, the request might go one of two ways: the request may find its destination URL and attempt to get the data in question, which would get returned into the .then() method; or if the request fails, that error will be sent to the .catch() method.
+        // Notice this `.catch()` getting chained onto the end of the `.then()` method
+        alert("Unable to connect to Github");
     });
     
 };
+
+// this function will accept both the array of repository data and the term we searched for as parameters
+var displayRepos = function(repos, searchTerm) {
+    
+    // check if api returned any repos - check for empty arrays and let's the user know if there's nothing to display
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+    console.log(repos);
+    console.log(searchTerm);
+
+    repoSearchTerm.textContent = searchTerm;
+    
+    // loop over repos - we're taking each repository (repos[i]) and writing some of its data to the page
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+        
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML =
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append container to the DOM
+        repoContainerEl.appendChild(repoEl);
+    }
+
+    // when the response data is converted to JSON, it will be sent from getUserRepos() to displayRepos()
+    response.json().then(function(data) {
+        displayRepos(data, user);
+    });
+    
+};
+
+//submit function - When we submit the form we get value from the <input> element via the nameInputEl DOM variable and store the value in its own variable called username
+var formSubmitHandler = function(event) {
+    // prevents the page from refreshing
+    event.preventDefault();
+    // console.log(event);
+    // get value from input element
+    var username = nameInputEl.value.trim(); // .trim() is useful when we leave a leading or trailing space in the <input> element
+
+    if (username) {
+        getUserRepos(username);
+        
+        // clear old content
+        repoContainerEl.textContent = "";
+        nameInputEl.value = "";
+        repoSearchTerm.textContent = searchTerm;
+    } else {
+        alert("Please enter a Github username");
+    }
+};
   
-  getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
